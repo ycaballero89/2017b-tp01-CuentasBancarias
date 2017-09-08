82bc20e3c4614c3ada0cdd69d7d223a969d282b5
Beatriz Yanina Caballero <caballero.yani@gmail.com>
2017-09-08 00:59:57 -0300

    VERDE: Se resuelve el caso base

--- test/cuentasBancarias/TestTransferencia.java
+++ test/cuentasBancarias/TestTransferencia.java
@@ -1,12 +1,11 @@
 package cuentasBancarias;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TestTransferencia {
 
 	@Test
 	public void test(){
-		fail("Not yet implemented");
+
 	}
 }
